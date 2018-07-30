8ddae9c9a9404eafcdfe047f9f767082a8cc8722
Christopher David <chris@arcade.city>
2018-07-29 21:14:29 -0500

    Suppress requiresMainQueueSetup warning

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 /** @format */
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+import { AppRegistry, YellowBox } from 'react-native'
+import App from './App'
+import { name as appName } from './app.json'
 
-AppRegistry.registerComponent(appName, () => App);
+YellowBox.ignoreWarnings([
+  'Module MobileAppBridge requires main queue'
+])
+
+AppRegistry.registerComponent(appName, () => App)
